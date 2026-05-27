e8cebd7185de7e9cfd04a0796e67d8ab03709fc6
Richard Guy <richardguy@deloitte.co.uk>
2026-05-27 14:23:28 +0100

    Remove tenancy_ocid variable from variables.tf

--- variables.tf
+++ variables.tf
@@ -2,12 +2,9 @@ variable "compartment_id" {
   description = "OCID from your tenancy page"
   type        = string
 }
+
 variable "region" {
   description = "region where you have OCI tenancy"
   type        = string
   default     = "us-sanjose-1"
-}
-variable "tenancy_ocid" {
-  description = "OCID from your tenancy page"
-  type        = string
 }
