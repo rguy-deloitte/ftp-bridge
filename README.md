cf1f8a24cc7e420c7b3a8983e2b633ca1f7ca1ce
Richard Guy <richardguy@deloitte.co.uk>
2026-05-29 14:53:02 +0100

    Added scheduling configuration

--- README.md
+++ README.md
@@ -4,29 +4,34 @@
 
 It is not possible to configure a direct connection between Oracle Fusion and HDFC bank as their SFTP server requires a kind of multi-factor authentication (key + passphrase) that is not supported by Oracle nor OIC. The ftp-bridge service has been created to resolve this issue. 
 
-![ftp-bridge diagram](./ftp-bridge.png)
+![ftp-bridge flow diagram](./ftp-bridge.png)
 
 ## Architecture
 
-A simple shell script copies files between the ftp servers. The script is called via a container running node and the [Fn serverless functions platform](https://fnproject.io/). The cloud resources are deployed via a [terraform template](./main.tf). The following resources are used:
+![ftp-bridge architecture diagram](./ftp-bridge-architecture.png)
+
+A simple [shell script](./ftp-bridge.sh) copies files between the ftp servers. The script is called via a container running node and the [Fn serverless functions platform](https://fnproject.io/). The cloud resources are deployed via a [terraform template](./main.tf). The following resources are used:
 
 | Resource      | Purpose |
 | ------------- |:-------------:|
-| Container Registry & Repository | Used to store the container image |
-| Private VCN & Subnet | Secure hosting of OIC Function Service |
-| NAT Gateway | Allows ftp-bridge service access to the internet without exposing it  to incoming internet connections |
-| Service Gateway | Allows ftp-bridge service access to required OCI services (eg. Container Registry) |
-| OCI Function | The ftp-bridge Service itself |
-
-## Building and running your application
+| [Container Registry & Repository](https://docs.oracle.com/en-us/iaas/Content/Registry/Concepts/registryoverview.htm) | Used to store the container image |
+| [Private VCN & Subnet](https://docs.oracle.com/en-us/iaas/Content/Network/Concepts/overview.htm) | Secure hosting of OIC Function Service |
+| [NAT Gateway](https://docs.oracle.com/en-us/iaas/Content/Network/Tasks/NATgateway.htm) | Allows ftp-bridge service access to the internet without exposing it  to incoming internet connections |
+| [Service Gateway](https://docs.oracle.com/en-us/iaas/Content/Network/Tasks/servicegateway.htm) | Allows ftp-bridge service access to required OCI services (eg. Container Registry) |
+| [OCI Function](https://docs.oracle.com/en-us/iaas/Content/Functions/Concepts/functionsoverview.htm) | The ftp-bridge Service itself (serverless function) |
+| [Resource Scheduler](https://docs.oracle.com/en-us/iaas/Content/resource-scheduler/home.htm) | Schedules the ftp-bridge service |
+| [Service Log](https://docs.oracle.com/en-us/iaas/Content/Logging/Concepts/loggingoverview.htm) | Logging for the ftp-bridge service |
 
 ### Prepare the Repo
 
 #### Create 'terraform.tfvars' file as follows
 
 ``` text
-compartment_id  = "<your_compartment_OCID_here>"
-region          = "uk-london-1"
+compartment_id      = "<your_compartment_OCID_here>"
+region              = "uk-london-1"
+tenancy_ocid        = "<your_tenancy_id_here>"
+logging_group_id    = "<your_logging_group_id_here>"
+image_path          = "<docker image path> (see below)"
 ```
 
 ### Deploy Docker Image
@@ -37,22 +42,30 @@ docker build --platform=linux/amd64 -t lhr.ocir.io/[repositoryNamespace]/ftp-bri
 docker push lhr.ocir.io/[repositoryNamespace]/ftp-bridge:0.1.0
 ```
 
-(you may want to move the docker image to correct compartment if this is your first time pushing)
-...and then update main.tf
+(you may want to move the docker image to correct compartment if this is your first time pushing) <br>
+Now update the `terraform.tfvars` file with the image path
 
 ### Deploy Infrastructure
 
 ``` shell
-oci session authenticate (Then choose - 70, then type DEFAULT)
-oci session authenticate (Then choose - 70, then type FTP-BRIDGE-TF)
-oci session refresh --profile FTP-BRIDGE-TF     -- to refresh the auth token
+oci session authenticate (then choose 70, then type DEFAULT)
+oci session authenticate (then choose 70, then type FTP-BRIDGE-TF)
 terraform apply
 ```
 
 ## Notes & Links
 
-- Tutorial: https://developer.hashicorp.com/terraform/tutorials/oci-get-started
-- https://registry.terraform.io/providers/oracle/oci/latest/docs
-- https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/functions_function
+- Terraform:
+    - https://docs.oracle.com/en-us/iaas/Content/dev/terraform/home.htm
+    - https://developer.hashicorp.com/terraform/tutorials/oci-get-started
+    - https://docs.oracle.com/en-us/iaas/Content/dev/terraform/tutorials/tf-simple-infrastructure.htm
+    - https://registry.terraform.io/providers/oracle/oci/latest/docs
+    - https://docs.oracle.com/en/cloud/foundation/iac/index.html#deployment-architectures
+- OCI CLI, Functions & Docker:
+    - https://developer.hashicorp.com/terraform/tutorials/oci-get-started/oci-build
+    - https://docs.oracle.com/en-us/iaas/Content/Functions/home.htm
+    - https://fnproject.io/tutorials/node/intro/
+    - https://docs.docker.com/get-started/
 - To list compartments: `oci iam compartment list --config-file /Users/[your username]]/.oci/config --profile DEFAULT --auth security_token --compartment-id-in-subtree true`
+- To refresh OCI auth token: `oci session refresh --profile FTP-BRIDGE-TF`
 - To manually invoke the function: `fn invoke ftp-bridge-application ftp-bridge-function`
--- main.tf
+++ main.tf
@@ -30,72 +30,37 @@ resource "oci_core_subnet" "ftpb_subnet" {
 }
 
 resource "oci_core_nat_gateway" "ftpb_nat_gateway" {
-    compartment_id = var.compartment_id
-    vcn_id = oci_core_vcn.ftpb_vcn.id
-    block_traffic = false
-    display_name = "ftp-bridge-natg"
+  compartment_id = var.compartment_id
+  vcn_id = oci_core_vcn.ftpb_vcn.id
+  block_traffic = false
+  display_name = "ftp-bridge-natg"
 }
 
 resource "oci_core_service_gateway" "ftpb_service_gateway" {
-    display_name = "ftp-bridge-servgate"
-    compartment_id = var.compartment_id
-    services {
-        service_id = "ocid1.service.oc1.uk-london-1.aaaaaaaatwg7f5mnzoapfunl66n2qkp4ormiykqk3hiwksum63gcyjk7ysla"
-    }
-    vcn_id = oci_core_vcn.ftpb_vcn.id
+  display_name = "ftp-bridge-servgate"
+  compartment_id = var.compartment_id
+  services {
+      service_id = "ocid1.service.oc1.uk-london-1.aaaaaaaatwg7f5mnzoapfunl66n2qkp4ormiykqk3hiwksum63gcyjk7ysla"
+  }
+  vcn_id = oci_core_vcn.ftpb_vcn.id
 }
-
-# data "oci_core_services" "test_services" {
-#   filter {
-#     name   = "name"
-#     values = ["All .* Services In Oracle Services Network"]
-#     regex  = true
-#   }
-# }
-
-# output "services" {
-#   value = [data.oci_core_services.test_services.services]
-# }
-
-# resource "oci_core_route_table" "ftpb_private_rt" {
-#     compartment_id = var.compartment_id
-#     vcn_id         = oci_core_vcn.ftpb_vcn.id
-#     display_name   = "ftp-bridge-private-rt"
-
-#     route_rules {
-#         destination_type  = "CIDR_BLOCK"
-#         destination       = "0.0.0.0/0"
-#         network_entity_id = oci_core_nat_gateway.ftpb_nat_gateway.id
-#     }
-
-#     route_rules {
-#         destination_type  = "SERVICE_CIDR_BLOCK"
-#         destination = "all-lhr-services-in-oracle-services-network"
-#         network_entity_id = oci_core_service_gateway.ftpb_service_gateway.id
-#     }
-# }
 
 resource "oci_core_default_route_table" "default-route-table" {
   compartment_id = var.compartment_id
   manage_default_resource_id = oci_core_vcn.ftpb_vcn.default_route_table_id
 
   route_rules {
-      destination_type  = "CIDR_BLOCK"
-      destination       = "0.0.0.0/0"
-      network_entity_id = oci_core_nat_gateway.ftpb_nat_gateway.id
+    destination_type  = "CIDR_BLOCK"
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_nat_gateway.ftpb_nat_gateway.id
   }
 
   route_rules {
-      destination_type  = "SERVICE_CIDR_BLOCK"
-      destination = "all-lhr-services-in-oracle-services-network"
-      network_entity_id = oci_core_service_gateway.ftpb_service_gateway.id
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    destination = "all-lhr-services-in-oracle-services-network"
+    network_entity_id = oci_core_service_gateway.ftpb_service_gateway.id
   }
 }
-
-# resource "oci_core_route_table_attachment" "ftpb_subnet_rt_attach" {
-#     subnet_id      = oci_core_subnet.ftpb_subnet.id
-#     route_table_id = oci_core_route_table.ftpb_private_rt.id
-# }
 
 resource "oci_functions_application" "ftpb_fnapplication" {
   compartment_id = var.compartment_id
@@ -105,16 +70,6 @@ resource "oci_functions_application" "ftpb_fnapplication" {
   image_policy_config {
     is_policy_enabled = false
   }
-
-  # trace_config {
-  #  domain_id  = var.application_trace_config.domain_id
-  #  is_enabled = var.application_trace_config.is_enabled
-  # }
-
-  # logging {
-      #Optional
-  #    line_format = var.application_logging_line_format
-  #  }
 }
 
 resource "oci_functions_function" "ftpb_function" {
@@ -129,9 +84,10 @@ resource "oci_functions_function" "ftpb_function" {
 }
 
 resource "oci_resource_scheduler_schedule" "ftpb-out-schedule" {
-  #Required
   action             = "START_RESOURCE"
   compartment_id     = var.compartment_id
+  description        = "Schedule to run ftp-bridge inbound transfer at 0 minutes past each hour"
+  display_name       = "ftp-bridge-inbound-hourly-0"
   recurrence_details = "0 * * * *"
   recurrence_type    = "CRON"
 
@@ -139,43 +95,33 @@ resource "oci_resource_scheduler_schedule" "ftpb-out-schedule" {
     id = oci_functions_function.ftpb_function.id
     parameters {
       parameter_type = "BODY"
-      value = [jsonencode({
-        source = ".hdfc.source.env"
-      })]
+      value = [jsonencode({ source = ".hdfc.source.env" })]
     }
   }
-
-  #Optional
-  description   = "Schedule to run ftp-bridge inbound transfer at 0 minutes past each hour"
-  display_name  = "ftp-bridge-inbound-hourly-0"
 }
 
 resource "oci_resource_scheduler_schedule" "ftpb-in-schedule-0" {
-  #Required
-  action             = "START_RESOURCE"
-  compartment_id     = var.compartment_id
-  recurrence_details = "10 * * * *"
-  recurrence_type    = "CRON"
+  action              = "START_RESOURCE"
+  compartment_id      = var.compartment_id
+  description         = "Schedule to run ftp-bridge inbound transfer at 10 minutes past each hour"
+  display_name        = "ftp-bridge-inbound-hourly-10"
+  recurrence_details  = "10 * * * *"
+  recurrence_type     = "CRON"
 
   resources {
     id = oci_functions_function.ftpb_function.id
     parameters {
       parameter_type = "BODY"
-      value = [jsonencode({
-        source = ".hdfc.source.env"
-      })]
+      value = [jsonencode({ source = ".hdfc.source.env" })]
     }
   }
-
-  #Optional
-  description   = "Schedule to run ftp-bridge inbound transfer at 10 minutes past each hour"
-  display_name  = "ftp-bridge-inbound-hourly-10"
 }
 
 resource "oci_resource_scheduler_schedule" "ftpb-in-schedule-1" {
-  #Required
   action             = "START_RESOURCE"
   compartment_id     = var.compartment_id
+  description        = "Schedule to run ftp-bridge inbound transfer at 40 minutes past each hour"
+  display_name       = "ftp-bridge-inbound-hourly-40"
   recurrence_details = "40 * * * *"
   recurrence_type    = "CRON"
 
@@ -183,64 +129,38 @@ resource "oci_resource_scheduler_schedule" "ftpb-in-schedule-1" {
     id = oci_functions_function.ftpb_function.id
     parameters {
       parameter_type = "BODY"
-      value = [jsonencode({
-        source = ".hdfc.source.env"
-      })]
+      value = [jsonencode({ source = ".hdfc.source.env" })]
     }
   }
-
-  #Optional
-  description   = "Schedule to run ftp-bridge inbound transfer at 40 minutes past each hour"
-  display_name  = "ftp-bridge-inbound-hourly-40"
 }
 
 resource "oci_identity_dynamic_group" "ftpb-dynamic_group" {
-    #Required
-    compartment_id = var.tenancy_ocid
-    description = "Dynamic group for ftp-bridge schedules which need permissions to invoke OCI functions"
-    matching_rule = "ANY {resource.id='${oci_resource_scheduler_schedule.ftpb-out-schedule.id}', resource.id='${oci_resource_scheduler_schedule.ftpb-in-schedule-0.id}', resource.id='${oci_resource_scheduler_schedule.ftpb-in-schedule-1.id}'}"
-    name = "ftp-bridge-schedule-dynamic-group"
+  compartment_id = var.tenancy_ocid
+  description = "Dynamic group for ftp-bridge schedules which need permissions to invoke OCI functions"
+  matching_rule = "ANY {resource.id='${oci_resource_scheduler_schedule.ftpb-out-schedule.id}', resource.id='${oci_resource_scheduler_schedule.ftpb-in-schedule-0.id}', resource.id='${oci_resource_scheduler_schedule.ftpb-in-schedule-1.id}'}"
+  name = "ftp-bridge-schedule-dynamic-group"
 }
 
 resource "oci_identity_policy" "ftpb-schedule-policy" {
-    #Required
-    compartment_id = var.tenancy_ocid
-    description = "Policy to allow ftp-bridge schedules to invoke OCI functions"
-    name = "ftp-bridge-schedule-policy"
-    statements = ["Allow dynamic-group ${oci_identity_dynamic_group.ftpb-dynamic_group.name} to manage functions-family in tenancy"]
+  compartment_id = var.tenancy_ocid
+  description = "Policy to allow ftp-bridge schedules to invoke OCI functions"
+  name = "ftp-bridge-schedule-policy"
+  statements = ["Allow dynamic-group ${oci_identity_dynamic_group.ftpb-dynamic_group.name} to manage functions-family in tenancy"]
 }
 
-# resource "oci_logging_log_group" "ftpb_log_group" {
-#   #Required
-#   compartment_id = var.compartment_id
-#   display_name   = "ftp-bridge-loggroup"
-
-#   lifecycle {
-#     ignore_changes = [ defined_tags ]
-#   }
-# }
-
 resource "oci_logging_log" "ftpb-log" {
-    #Required
-    display_name = "ftp-bridge-log"
-    log_group_id = var.logging_group_id
-    log_type = "SERVICE"
+  display_name = "ftp-bridge-log"
+  log_group_id = var.logging_group_id
+  log_type = "SERVICE"
+  is_enabled = "true"
+  retention_duration = "30"
 
-    #Optional
-    configuration {
-        #Required
-        source {
-            #Required
-            category = "invoke"
-            resource = oci_functions_application.ftpb_fnapplication.id
-            service = "Functions"
-            source_type = "OCISERVICE"
-
-            #Optional
-            #parameters = var.log_configuration_source_parameters
-        }
+  configuration {
+    source {
+      category = "invoke"
+      resource = oci_functions_application.ftpb_fnapplication.id
+      service = "Functions"
+      source_type = "OCISERVICE"
     }
-
-    is_enabled = "true"
-    retention_duration = "30"
+  }
 }
